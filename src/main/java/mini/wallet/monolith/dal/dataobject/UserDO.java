30907727fb02298804a3d3234e7ddea7b701b8f0
install18 <install.delapanbelas@gmail.com>
2023-01-15 21:28:38 +0700

    Add UserDO

---
+++ src/main/java/mini/wallet/monolith/dal/dataobject/UserDO.java
@@ -0,0 +1,38 @@
+package mini.wallet.monolith.dal.dataobject;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import mini.wallet.monolith.model.base.BaseDataObject;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class UserDO extends BaseDataObject {
+    @Id
+    @Column(updatable = false)
+    private String userId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String fullName;
+}
