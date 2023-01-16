b788b28d3116576da9575f33ddf677afa97354c2
hansen.situmorang <hansen.situmorang@dana.id>
2023-01-16 17:11:56 +0700

    Add lombok annotation to BaseDataObject

--- src/main/java/mini/wallet/monolith/model/base/BaseDataObject.java
+++ src/main/java/mini/wallet/monolith/model/base/BaseDataObject.java
@@ -1,9 +1,15 @@
 package mini.wallet.monolith.model.base;
 
 import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BaseDataObject {
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdDateTime;
