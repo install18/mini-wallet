0cae22cbe60b502d9bb007656fbdae42fe488335
hansen.situmorang <hansen.situmorang@dana.id>
2023-01-16 19:44:53 +0700

    Add logic to add user and wallet instead of just user for registration

--- src/main/java/mini/wallet/monolith/controller/RegisterController.java
+++ src/main/java/mini/wallet/monolith/controller/RegisterController.java
@@ -1,8 +1,13 @@
 package mini.wallet.monolith.controller;
 
+import mini.wallet.monolith.model.request.RegisterRequest;
 import mini.wallet.monolith.model.request.UserRegisterRequest;
+import mini.wallet.monolith.model.request.WalletRegisterRequest;
+import mini.wallet.monolith.model.response.RegisterResponse;
 import mini.wallet.monolith.model.response.UserRegisterResponse;
+import mini.wallet.monolith.model.response.WalletRegisterResponse;
 import mini.wallet.monolith.service.user.UserService;
+import mini.wallet.monolith.service.user.WalletService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +17,52 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/register")
 public class RegisterController {
     private final UserService userService;
+    private final WalletService walletService;
 
-    public RegisterController(UserService userService) {
+    public RegisterController(UserService userService, WalletService walletService) {
         this.userService = userService;
+        this.walletService = walletService;
     }
 
     @PostMapping
-    public UserRegisterResponse register(@RequestBody UserRegisterRequest userRegisterRequest) {
-        return userService.register(userRegisterRequest);
+    public RegisterResponse register(@RequestBody RegisterRequest registerRequest) {
+        UserRegisterRequest userRegisterRequest = buildUserRegisterRequest(registerRequest);
+        UserRegisterResponse userRegisterResponse = userService.register(userRegisterRequest);
+
+        WalletRegisterRequest walletRegisterRequest = buildWalletRegisterRequest(userRegisterResponse);
+        WalletRegisterResponse walletRegisterResponse = walletService.register(walletRegisterRequest);
+
+
+        return buildRegisterResponse(userRegisterResponse, walletRegisterResponse);
+    }
+
+    private UserRegisterRequest buildUserRegisterRequest(RegisterRequest registerRequest) {
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+
+        userRegisterRequest.setEmail(registerRequest.getEmail());
+        userRegisterRequest.setAddress(registerRequest.getAddress());
+        userRegisterRequest.setPassword(registerRequest.getPassword());
+        userRegisterRequest.setFullName(registerRequest.getFullName());
+        userRegisterRequest.setPhoneNumber(registerRequest.getPhoneNumber());
+
+        return userRegisterRequest;
+    }
+
+    private WalletRegisterRequest buildWalletRegisterRequest(UserRegisterResponse userRegisterResponse) {
+        WalletRegisterRequest walletRegisterRequest = new WalletRegisterRequest();
+
+        walletRegisterRequest.setUserId(userRegisterResponse.getUser().getUserId());
+
+        return walletRegisterRequest;
+    }
+
+    private RegisterResponse buildRegisterResponse(UserRegisterResponse userRegisterResponse,
+                                                   WalletRegisterResponse walletRegisterResponse) {
+        RegisterResponse registerResponse = new RegisterResponse();
+
+        registerResponse.setUser(userRegisterResponse.getUser());
+        registerResponse.setWallet(walletRegisterResponse.getWallet());
+
+        return registerResponse;
     }
 }
