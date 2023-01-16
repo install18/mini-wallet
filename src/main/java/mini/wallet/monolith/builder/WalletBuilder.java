2b6ec77e13c81037f1650f104c2e612892b5325d
hansen.situmorang <hansen.situmorang@dana.id>
2023-01-16 19:33:45 +0700

    Add WalletBuilder

---
+++ src/main/java/mini/wallet/monolith/builder/WalletBuilder.java
@@ -0,0 +1,28 @@
+package mini.wallet.monolith.builder;
+
+import mini.wallet.monolith.dal.dataobject.WalletDO;
+import mini.wallet.monolith.model.Wallet;
+import mini.wallet.monolith.model.request.WalletRegisterRequest;
+
+import java.util.UUID;
+
+public class WalletBuilder {
+    public static WalletDO buildWalletDOFromWalletRegisterRequest(WalletRegisterRequest walletRegisterRequest) {
+        WalletDO walletDO = new WalletDO();
+
+        walletDO.setWalletId(UUID.randomUUID().toString());
+        walletDO.setUserId(walletRegisterRequest.getUserId());
+        walletDO.setBalance("0");
+
+        return walletDO;
+    }
+
+    public static Wallet buildWalletFromWalletDO(WalletDO walletDO) {
+        Wallet wallet = new Wallet();
+
+        wallet.setWalletId(walletDO.getWalletId());
+        wallet.setBalance(walletDO.getBalance());
+
+        return wallet;
+    }
+}
