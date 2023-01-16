2bc9e170223b523e6236f7416828d9dd7bfee107
hansen.situmorang <hansen.situmorang@dana.id>
2023-01-16 19:43:48 +0700

    Add userId to model User and change method name

--- src/main/java/mini/wallet/monolith/builder/UserBuilder.java
+++ src/main/java/mini/wallet/monolith/builder/UserBuilder.java
@@ -7,7 +7,7 @@ import mini.wallet.monolith.model.request.UserRegisterRequest;
 import java.util.UUID;
 
 public class UserBuilder {
-    public static UserDO buildUserDOFromRegisterRequest(UserRegisterRequest userRegisterRequest) {
+    public static UserDO buildUserDOFromUserRegisterRequest(UserRegisterRequest userRegisterRequest) {
         UserDO userDO = new UserDO();
 
         userDO.setUserId(UUID.randomUUID().toString());
@@ -23,6 +23,7 @@ public class UserBuilder {
     public static User buildUserFromUserDO(UserDO userDO) {
         User user = new User();
 
+        user.setUserId(userDO.getUserId());
         user.setEmail(userDO.getEmail());
         user.setAddress(userDO.getAddress());
         user.setFullName(userDO.getFullName());
