6b574e7fb665a0076718740edb5fc635a8cb19bd
hansen.situmorang <hansen.situmorang@dana.id>
2023-02-03 19:41:17 +0700

    Add data object for wallet

---
+++ src/main/java/mini/wallet/monolith/dal/dataobject/WalletDO.java
@@ -0,0 +1,32 @@
+package mini.wallet.monolith.dal.dataobject;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import mini.wallet.monolith.model.base.BaseDataObject;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "`wallet`")
+public class WalletDO extends BaseDataObject {
+    @Id
+    @Column(updatable = false)
+    private String walletId;
+
+    @NotBlank
+    private String customerId;
+
+    @NotBlank
+    private String balance;
+}
