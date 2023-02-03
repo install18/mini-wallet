58e11dee7290bb2ac44a6c19273b24b1949073ef
hansen.situmorang <hansen.situmorang@dana.id>
2023-02-03 19:50:18 +0700

    Add login controller

---
+++ src/main/java/mini/wallet/monolith/controller/api/LoginController.java
@@ -0,0 +1,25 @@
+package mini.wallet.monolith.controller.api;
+
+
+import mini.wallet.monolith.model.request.LoginRequest;
+import mini.wallet.monolith.model.response.LoginResponse;
+import mini.wallet.monolith.service.CustomerService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/login")
+public class LoginController {
+    private final CustomerService customerService;
+
+    public LoginController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public LoginResponse login(@RequestBody LoginRequest loginRequest) {
+        return customerService.login(loginRequest);
+    }
+}
