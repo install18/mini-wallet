36eee7680c20a648959015812c1d765825b8f276
hansen.situmorang <hansen.situmorang@dana.id>
2023-02-03 19:42:50 +0700

    Add repository for transaction history

---
+++ src/main/java/mini/wallet/monolith/dal/repository/TransactionRepository.java
@@ -0,0 +1,12 @@
+package mini.wallet.monolith.dal.repository;
+
+import mini.wallet.monolith.dal.dataobject.TransactionDO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<TransactionDO, String> {
+    Optional<TransactionDO> queryByCustomerId(String customerId);
+}
