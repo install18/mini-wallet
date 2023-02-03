805216ad2cf92da6162f59c5f90c872e78820d9e
hansen.situmorang <hansen.situmorang@dana.id>
2023-02-03 19:49:42 +0700

    Add wallet service

---
+++ src/main/java/mini/wallet/monolith/service/WalletService.java
@@ -0,0 +1,32 @@
+package mini.wallet.monolith.service;
+
+import jakarta.transaction.Transactional;
+import mini.wallet.monolith.dal.dataobject.WalletDO;
+import mini.wallet.monolith.dal.repository.WalletRepository;
+import mini.wallet.monolith.helper.builder.WalletBuilder;
+import mini.wallet.monolith.model.Wallet;
+import mini.wallet.monolith.model.request.WalletRegisterRequest;
+import mini.wallet.monolith.model.response.WalletRegisterResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WalletService {
+    private final WalletRepository walletRepository;
+
+    public WalletService(WalletRepository walletRepository) {
+        this.walletRepository = walletRepository;
+    }
+
+    @Transactional
+    public WalletRegisterResponse register(WalletRegisterRequest walletRegisterRequest) {
+        WalletDO walletDO = WalletBuilder.buildWalletDOFromWalletRegisterRequest(walletRegisterRequest);
+        walletRepository.save(walletDO);
+
+        Wallet wallet = WalletBuilder.buildWalletFromWalletDO(walletDO);
+
+        WalletRegisterResponse walletRegisterResponse = new WalletRegisterResponse();
+        walletRegisterResponse.setWallet(wallet);
+
+        return walletRegisterResponse;
+    }
+}
