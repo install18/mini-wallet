9b873ab36d0250423f50591e6f7dcf69ad44a379
hansen.situmorang <hansen.situmorang@dana.id>
2023-02-03 19:37:00 +0700

    Add data object for customer

---
+++ src/main/java/mini/wallet/monolith/dal/dataobject/CustomerDO.java
@@ -0,0 +1,45 @@
+package mini.wallet.monolith.dal.dataobject;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import mini.wallet.monolith.model.base.BaseDataObject;
+
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "`customer`")
+public class CustomerDO extends BaseDataObject {
+    @Id
+    @Column(updatable = false)
+    private String customerId;
+
+    @NotBlank
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    @Email
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    private String address;
+
+    private String fullName;
+
+    private LocalDateTime birthDate;
+}
