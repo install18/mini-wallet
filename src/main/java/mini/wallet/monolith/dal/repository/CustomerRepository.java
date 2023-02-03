ff0c6fd03b5e60286da9c06b790c6275bce42d14
hansen.situmorang <hansen.situmorang@dana.id>
2023-02-03 19:45:33 +0700

    Add repository for customer

---
+++ src/main/java/mini/wallet/monolith/dal/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package mini.wallet.monolith.dal.repository;
+
+import mini.wallet.monolith.dal.dataobject.CustomerDO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<CustomerDO, String> {
+    Optional<CustomerDO> queryByPhoneNumber(String phoneNumber);
+
+    Optional<CustomerDO> queryByEmail(String email);
+}
